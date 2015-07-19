8ccffdcf96294db2d31baef729ade1d0811e86bc
nupatel@outlook.com <nupatel@outlook.com>
2015-07-19 14:45:01 -0500

    completed Peer Assessment 1 file

--- PA1_template.R
+++ PA1_template.R
@@ -1,59 +1,45 @@
-Peer Assessment 1
-=================
+setwd("C:/Users/Neil/SkyDrive/Data Science/Reproducible Research/repdata-data-activity")
 
-
-Loading in the data and creating an identical dataset with ommitted missing values
-```{r, echo = TRUE}
+## loads in the "activity" dataset and removes missing values
 data <- read.csv("activity.csv", header = TRUE, sep = ",") 
 cleandata <- data[!is.na(data),]
-```
 
-Aggregate total steps by data:
-```{r, echo = TRUE}
+## aggregates total steps by date
 steps_per_day <- aggregate(steps ~ date, data = cleandata, FUN = sum)
-```
 
-Calculates mean and median of the total number of steps taken per day
-```{r, echo = TRUE}
+## calculates mean and median of the total number of steps taken per day
 stepsMean <- mean(steps_per_day$steps)
 print(stepsMean)
 stepsMedian <- median(steps_per_day$steps)
 print(stepsMedian)
-```
 
-Creates histogram of total steps taken per day
-```{r, echo = TRUE}
+## creates histogram of steps taken per day
 library(ggplot2)
 
+png("fig1.png", width = 1000, height = 500)
 ggplot(data = steps_per_day, aes(x = steps_per_day$steps)) + geom_histogram(fill = "steelblue", binwidth = 1000) + 
     xlab("Steps per Day") + ggtitle("Distribution of Steps per Day") + geom_vline(xintercept = stepsMean, size = 1) +
     theme(plot.title = element_text(size = 20, face = "bold", vjust = 2))
-```
+dev.off()
 
-Creates a time series plot of the average number of steps taken (averaged across all days) versus the 5-minute intervals
-```{r, echo = TRUE}
+## creates time series plot of average number of steps taken per 5-minute interval
 steps_per_interval <- aggregate(steps ~ interval, data = cleandata, FUN = mean)
 
+png("fig2.png", width = 1000, height = 500)
 ggplot(data = steps_per_interval, aes(x = interval, y = steps)) + geom_line(color = "steelblue", size = 1) + 
     xlab("Interval") + ylab("Steps") + ggtitle("Average Number of Steps per 5 Minute Interval") +
     theme(plot.title = element_text(size = 20, face = "bold", vjust = 2))
-```
+dev.off()
 
-Calculates which interval has, on average, the maximum number of steps
-```{r, echo = TRUE}
+## calculates which interval has, on average, the maximum number of steps
 maxSteps <- max(steps_per_interval$steps)
 maxSteps_interval <- steps_per_interval$interval[which(steps_per_interval$steps == maxSteps)]
 print(maxSteps_interval)
-```
 
-Calculates and reports the total number of missing values in the dataset
-```{r, echo = TRUE}
+## calculates and reports the total number of missing values in the dataset
 num_NA <- sum(is.na(data))
 print(num_NA)
-```
 
-Replaces missing data with the step means of their respective intervals
-```{r, echo = TRUE}
 ## replaces NA values with the means for that interval
 data_filledNA <- data
 for(i in 1:nrow(data_filledNA)){
@@ -62,36 +48,31 @@ for(i in 1:nrow(data_filledNA)){
         data_filledNA$steps[i] <- steps_per_interval$steps[steps_per_interval$interval == intervalIndex]
     }
 }
-```
 
-Creates a histogram of the total number of steps taken each day with the Data_filledNA dataset
-```{r, echo=TRUE}
+## creates a histogram of the total number of steps taken each day with the Data_filledNA dataset
 steps_per_day_filledNA <- aggregate(steps ~ date, data = data_filledNA, FUN = sum)
-
+png("fig3.png", width = 1000, height = 500)
 ggplot(data = steps_per_day_filledNA, aes(x = steps_per_day_filledNA$steps)) + geom_histogram(fill = "steelblue", binwidth = 1000) + 
     xlab("Steps per Day") + ggtitle("Distribution of Steps per Day") + geom_vline(xintercept = mean(steps_per_day_filledNA$steps), size = 1) +
     theme(plot.title = element_text(size = 20, face = "bold", vjust = 2))
-```
+dev.off()
 
-Creates a new factor variable in the dataset with "weekday" and "weekend" levels 
-```{r, echo=TRUE}
+## Creates a new factor variable in the dataset with "weekday" and "weekend" levels 
 data_filledNA$date <- as.Date(data_filledNA$date)
-
 for(i in 1:nrow(data_filledNA)){
     if(weekdays(data_filledNA$date[i]) == "Saturday" | weekdays(data_filledNA$date[i]) == "Saturday") {
         data_filledNA$dayType[i] = "weekend"
     }
     else data_filledNA$dayType[i] = "weekday"
 }
-```
 
-Creates a panel plot containing a time series plot of the 5-minute intercal and the average number of steps taken, averaged across all weekdays/weekends
-```{r, echo = TRUE}
+## creates a panel plot containing a time series plot of the 5-minute intercal and the average number of steps taken, averaged across all weekdays/weekends
 steps_per_interval_filledNA <- aggregate(steps ~ interval + dayType, data = data_filledNA, FUN = mean)
-                                         
+
+png("fig4.png", width = 1000, height = 500)
 ggplot(data = steps_per_interval_filledNA, aes(x = interval, y = steps)) + 
     geom_line(color = "steelblue", size = 1) + 
     facet_grid(dayType ~ .) + 
     xlab("Interval") + ylab("Steps") + ggtitle("Average Number of Steps per 5 Minute Interval") +
     theme(plot.title = element_text(size = 20, face = "bold", vjust = 2))
-```
+dev.off()
--- PA1_template.md
+++ PA1_template.md
@@ -3,26 +3,41 @@ Peer Assessment 1
 
 
 Loading in the data and creating an identical dataset with ommitted missing values
-```{r, echo = TRUE}
+
+```r
 data <- read.csv("activity.csv", header = TRUE, sep = ",") 
 cleandata <- data[!is.na(data),]
 ```
 
 Aggregate total steps by data:
-```{r, echo = TRUE}
+
+```r
 steps_per_day <- aggregate(steps ~ date, data = cleandata, FUN = sum)
 ```
 
 Calculates mean and median of the total number of steps taken per day
-```{r, echo = TRUE}
+
+```r
 stepsMean <- mean(steps_per_day$steps)
 print(stepsMean)
+```
+
+```
+## [1] 10766.19
+```
+
+```r
 stepsMedian <- median(steps_per_day$steps)
 print(stepsMedian)
 ```
 
+```
+## [1] 10765
+```
+
 Creates histogram of total steps taken per day
-```{r, echo = TRUE}
+
+```r
 library(ggplot2)
 
 ggplot(data = steps_per_day, aes(x = steps_per_day$steps)) + geom_histogram(fill = "steelblue", binwidth = 1000) + 
@@ -30,8 +45,11 @@ ggplot(data = steps_per_day, aes(x = steps_per_day$steps)) + geom_histogram(fill
     theme(plot.title = element_text(size = 20, face = "bold", vjust = 2))
 ```
 
+![plot of chunk unnamed-chunk-4](figure/unnamed-chunk-4-1.png) 
+
 Creates a time series plot of the average number of steps taken (averaged across all days) versus the 5-minute intervals
-```{r, echo = TRUE}
+
+```r
 steps_per_interval <- aggregate(steps ~ interval, data = cleandata, FUN = mean)
 
 ggplot(data = steps_per_interval, aes(x = interval, y = steps)) + geom_line(color = "steelblue", size = 1) + 
@@ -39,21 +57,34 @@ ggplot(data = steps_per_interval, aes(x = interval, y = steps)) + geom_line(colo
     theme(plot.title = element_text(size = 20, face = "bold", vjust = 2))
 ```
 
+![plot of chunk unnamed-chunk-5](figure/unnamed-chunk-5-1.png) 
+
 Calculates which interval has, on average, the maximum number of steps
-```{r, echo = TRUE}
+
+```r
 maxSteps <- max(steps_per_interval$steps)
 maxSteps_interval <- steps_per_interval$interval[which(steps_per_interval$steps == maxSteps)]
 print(maxSteps_interval)
 ```
 
+```
+## [1] 835
+```
+
 Calculates and reports the total number of missing values in the dataset
-```{r, echo = TRUE}
+
+```r
 num_NA <- sum(is.na(data))
 print(num_NA)
 ```
 
+```
+## [1] 2304
+```
+
 Replaces missing data with the step means of their respective intervals
-```{r, echo = TRUE}
+
+```r
 ## replaces NA values with the means for that interval
 data_filledNA <- data
 for(i in 1:nrow(data_filledNA)){
@@ -65,7 +96,8 @@ for(i in 1:nrow(data_filledNA)){
 ```
 
 Creates a histogram of the total number of steps taken each day with the Data_filledNA dataset
-```{r, echo=TRUE}
+
+```r
 steps_per_day_filledNA <- aggregate(steps ~ date, data = data_filledNA, FUN = sum)
 
 ggplot(data = steps_per_day_filledNA, aes(x = steps_per_day_filledNA$steps)) + geom_histogram(fill = "steelblue", binwidth = 1000) + 
@@ -73,8 +105,11 @@ ggplot(data = steps_per_day_filledNA, aes(x = steps_per_day_filledNA$steps)) + g
     theme(plot.title = element_text(size = 20, face = "bold", vjust = 2))
 ```
 
+![plot of chunk unnamed-chunk-9](figure/unnamed-chunk-9-1.png) 
+
 Creates a new factor variable in the dataset with "weekday" and "weekend" levels 
-```{r, echo=TRUE}
+
+```r
 data_filledNA$date <- as.Date(data_filledNA$date)
 
 for(i in 1:nrow(data_filledNA)){
@@ -86,7 +121,8 @@ for(i in 1:nrow(data_filledNA)){
 ```
 
 Creates a panel plot containing a time series plot of the 5-minute intercal and the average number of steps taken, averaged across all weekdays/weekends
-```{r, echo = TRUE}
+
+```r
 steps_per_interval_filledNA <- aggregate(steps ~ interval + dayType, data = data_filledNA, FUN = mean)
                                          
 ggplot(data = steps_per_interval_filledNA, aes(x = interval, y = steps)) + 
@@ -95,3 +131,5 @@ ggplot(data = steps_per_interval_filledNA, aes(x = interval, y = steps)) +
     xlab("Interval") + ylab("Steps") + ggtitle("Average Number of Steps per 5 Minute Interval") +
     theme(plot.title = element_text(size = 20, face = "bold", vjust = 2))
 ```
+
+![plot of chunk unnamed-chunk-11](figure/unnamed-chunk-11-1.png) 
